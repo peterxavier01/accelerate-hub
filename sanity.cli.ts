1fa0d11d26e0e298c77e203ba8e7267c1f63d1ab
Peter Uadiale <fxavy02@gmail.com>
2024-11-02 23:34:07 +0100

    refactor: add studio host credentials to automate sanity studio deployment

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,5 +6,6 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const studioHost = process.env.NEXT_PUBLIC_SANITY_STUDIO_HOST
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({ api: { projectId, dataset }, studioHost })
