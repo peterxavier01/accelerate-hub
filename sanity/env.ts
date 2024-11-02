87c905beba985d5607e246adf5c1d55f57e13a54
Peter Uadiale <fxavy02@gmail.com>
2024-11-02 23:45:23 +0100

    refactor: expose env to sanity

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,17 +1,17 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-31'
+  process.env.SANITY_STUDIO_API_VERSION || '2024-10-31'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.SANITY_STUDIO_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.SANITY_STUDIO_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
-export const token = process.env.SANITY_WRITE_TOKEN
+export const token = process.env.SANITY_STUDIO_WRITE_TOKEN
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
